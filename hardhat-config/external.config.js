4550b42bc069ff8cb1a70f5e49121801c6f45a33
Nicolas Mahe <nicolas@mahe.me>
2024-07-19 14:36:50 +0700

    add ethereum-universal-forwarder v0.1.4 dependency

--- hardhat-config/external.config.js
+++ hardhat-config/external.config.js
@@ -8,6 +8,11 @@ module.exports = {
         artifacts: ['artifacts_previous/v4', 'artifacts_previous/v3'],
       },
       {
+        // import artifacts and deploy script from ethereum-universal-forwarder
+        artifacts: 'node_modules/ethereum-universal-forwarder/export/artifacts',
+        deploy: 'node_modules/ethereum-universal-forwarder/export/deploy',
+      },
+      {
         artifacts: 'node_modules/@animoca/ethereum-contracts/artifacts',
       },
     ],
